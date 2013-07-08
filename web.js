f234df70259626c1f44c9233c3c95a79b95ff2fe
Michael Toback <michael.toback@gmail.com>
2013-07-07 20:25:35 -0700

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+buffer = fs.readFileSync("index.html")
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.ToString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
