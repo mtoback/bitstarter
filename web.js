78efb36ced1fc0dea103c8230cddd9b82b91adb9
Michael Toback <michael.toback@gmail.com>
2013-07-07 20:36:34 -0700

    fix error in web.js... buffer.ToString --> buffer.toString

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 buffer = fs.readFileSync("index.html")
 app.get('/', function(request, response) {
-  response.send(buffer.ToString('utf-8'));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
